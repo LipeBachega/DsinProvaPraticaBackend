c2e2fbeb704260332b1d9e4f7180a7013ed82a99
LipeBachega <lipe.bachega@outlook.com>
2026-05-17 13:17:37 -0300

    feat: configurando Sequelize

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,11 +1,10 @@
-import { Sequelize } from 'sequelize';
-import path from 'path';
+import { Sequelize } from "sequelize";
+import path from "path";
 
-// O SQLite vai criar um arquivo chamado 'database.sqlite' dentro da pasta src/config
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, '..', 'database.sqlite'),
-  logging: false, // Desative se os logs SQL poluírem demais o terminal
+  dialect: "sqlite",
+  storage: path.join(__dirname, "..", "database.sqlite"),
+  logging: false,
 });
 
 export default sequelize;
