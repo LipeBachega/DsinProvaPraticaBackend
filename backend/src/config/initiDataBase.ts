682c3196df2cba2168b127106c3441a3f20b3b5c
LipeBachega <lipe.bachega@outlook.com>
2026-05-17 15:32:17 -0300

    feat: criando arquivo de inicializacao das tabelas

---
+++ backend/src/config/initiDataBase.ts
@@ -0,0 +1,18 @@
+import sequelize from "./database.js";
+import Customer from "../models/customer.model.js";
+import Service from "../models/service.model.js";
+import Appointment from "../models/appointment.model.js";
+import AppointmentService from "../models/appointmentService.model.js";
+
+export async function initDatabase() {
+  try {
+    console.log("Conectando e sincronizando o banco de dados SQLite...");
+
+    await sequelize.sync({ alter: true });
+
+    console.log("Banco de dados SQLite sincronizado e pronto para uso!");
+  } catch (error) {
+    console.error("Erro ao sincronizar o banco de dados:", error);
+    throw error;
+  }
+}
