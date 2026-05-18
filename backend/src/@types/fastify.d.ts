5e0e8b90a0201c25f2aaba4a3ee31ff088933c57
LipeBachega <lipe.bachega@outlook.com>
2026-05-18 16:04:30 -0300

    fix: corrigindo UserRole para IUserRole

--- backend/src/@types/fastify.d.ts
+++ backend/src/@types/fastify.d.ts
@@ -1,15 +1,11 @@
-import type {
-  UserRole,
-} from "../types/customer.type.js";
+import type { IUserRole } from "../types/customer.type.js";
 
 declare module "fastify" {
-
   interface FastifyRequest {
-
     user: {
       id: number;
       email: string;
-      role: UserRole;
+      role: IUserRole;
     };
   }
 }
