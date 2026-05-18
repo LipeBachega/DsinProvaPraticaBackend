619208206f647bd6c4f1b06c41f5100ac7c723ae
LipeBachega <lipe.bachega@outlook.com>
2026-05-18 14:43:07 -0300

    feat: adicionando interface de usuario autenticado

--- backend/src/types/auth.type.ts
+++ backend/src/types/auth.type.ts
@@ -1,4 +1,12 @@
+import type { IUserRole } from "./customer.type.js";
+
 export interface ILogin {
   email: string;
   password: string;
+}
+
+export interface IAuthenticatedUser {
+  id: number;
+  email: string;
+  role: IUserRole;
 }
